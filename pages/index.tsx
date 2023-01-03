76171eef503806beb6dffb46db4dd93177b01a1f
Darth Degen <darthdegen@gmail.com>
2023-01-03 08:38:12 -0700

    updated to landing to match mobile and desktop designs

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import { NextPage } from "next";
 import Image from "next/image";
 import bg from "public/images/landing-slimes-lg.png";
 import bgMobile from "public/images/landing-slimes-sm.png";
+import slimes from "public/images/slimes-text.png";
 
 const Home: NextPage = () => {
   const [didMount, setDidMount] = useState<boolean>(false);
@@ -17,26 +18,37 @@ const Home: NextPage = () => {
   return (
     <PageLayout>
       {didMount && (
-        <div className="h-full flex flex-col md:justify-between items-center">
-          <div className="z-10 pb-10">
-            <LogoText />
+        <div className="flex flex-col-reverse lg:flex-col lg:justify-center items-center">
+          <div className="z-10 pt-0 lg:pb-10 px-20 lg:px-0 lg:pt-48">
+            {/* <LogoText /> */}
+            <Image
+              src={slimes.src}
+              height={136.8}
+              width={400}
+              alt="My Slimes Text"
+            />
           </div>
-          <div className="hidden md:block px- z-0">
-            <Image src={bg.src} height={1151} width={2819} alt="My Slimes" />
+          <div className="hidden lg:block px-0 z-0 absolute bottom-0">
+            <Image
+              src={bg.src}
+              height={766}
+              width={2371}
+              alt="My Slimes Banner"
+            />
           </div>
-          <div className="md:hidden px-10 z-0 h-full">
+          <div className="lg:hidden px-0 z-0 pt-10 md:pt-16">
             <Image
               src={bgMobile.src}
               height={352.5}
               width={440.5}
-              alt="My Slimes"
+              alt="My Slimes Mobile"
             />
           </div>
 
-          {/* <div className="hidden md:block absolute bottom-0 px-10 z-0">
+          {/* <div className="hidden lg:block absolute bottom-0 px-10 z-0">
             <Image src={bg.src} height={1151} width={2819} alt="My Slimes" />
           </div>
-          <div className="md:hidden absolute bottom-0 px-10 z-0">
+          <div className="lg:hidden absolute bottom-0 px-10 z-0">
             <Image
               src={bgMobile.src}
               height={352.5}
--- src/components/templates/PageLayout.tsx
+++ src/components/templates/PageLayout.tsx
@@ -10,14 +10,11 @@ interface Props {
 const PageLayout: FC<Props> = (props: Props) => {
   const { children } = props;
   return (
-    <motion.div
-      className="flex flex-col justify-between min-h-screen"
-      {...enterAnimation}
-    >
+    <motion.div className="flex flex-col min-h-screen " {...enterAnimation}>
       <PageHead title="My Slimes" description="Welcome to My Slimes" />
 
       <Header />
-      <main className="flex flex-col justify-start items-center h-full z-0">
+      <main className="flex flex-col justify-start items-center h-full z-0 ">
         {children}
       </main>
 
