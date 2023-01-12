cc274bb568c4ae916018946c8c6f9ac12e773902
Darth Degen <darthdegen@gmail.com>
2023-01-12 13:49:05 -0700

    moved Head to root file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 import "tailwindcss/tailwind.css";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { PageHead } from "@components";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <PageHead title={"Slimes"} description="An art project by scum" />
+      <Component {...pageProps} />;
+    </>
+  );
 }
