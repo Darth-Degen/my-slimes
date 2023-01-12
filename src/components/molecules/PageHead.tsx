b1672728a2b492a64e33646bb8536868838926da
Darth Degen <darthdegen@gmail.com>
2023-01-11 19:55:23 -0700

    updated url

--- src/components/molecules/PageHead.tsx
+++ src/components/molecules/PageHead.tsx
@@ -19,20 +19,14 @@ const PageHead: FC<Props> = (props: Props) => {
       <meta name="twitter:site" content="@MySlimes_" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content="/images/meta.png" />
+      <meta name="twitter:image" content="meta.png" />
+      <meta property="twitter:url" content={`https://slimes.xyz/`} />
       {/* <!-- Facebook --> */}
-      {/* <meta property="og:type" content="website" />
+      <meta property="og:type" content="website" />
       <meta property="og:url" content={`https://slimes.xyz/`} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      <meta property="og:image" content={`/images/meta.png`} /> */}
-
-      {/* <!-- Twitter --> */}
-      {/* <meta name="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content={`https://slimes.xyz/`} />
-      <meta property="twitter:title" content={title} />
-      <meta property="twitter:description" content={description} />
-      <meta property="twitter:image" content={`/images/meta.png`} /> */}
+      <meta property="og:image" content={`meta.png`} />
     </Head>
   );
 };
