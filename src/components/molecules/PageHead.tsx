cffd1b5c1edc686d4382564e3d965b8e48301b4f
Darth Degen <darthdegen@gmail.com>
2023-01-12 14:11:19 -0700

    udpatad meta to old settings

--- src/components/molecules/PageHead.tsx
+++ src/components/molecules/PageHead.tsx
@@ -15,20 +15,17 @@ const PageHead: FC<Props> = (props: Props) => {
       <meta name="description" content={description} />
       <link rel="icon" href="/favicon.ico" />
       {/* twitter */}
-      <meta property="twitter:card" content="summary_large_image" />
+      {/* <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:url" content={`https://slimes.xyz`} />
       <meta property="twitter:title" content={title} />
       <meta property="twitter:description" content={description} />
-      <meta
-        property="twitter:image"
-        content={`https://www.slimes.xyz/meta.png`}
-      />
-      {/* <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:image" content={`/meta.png`} /> */}
+      <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:site" content="@MySlimes_" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content="/meta.png" />
-      <meta property="twitter:url" content={`https://www.slimes.xyz/`} /> */}
+      <meta name="twitter:image" content="https://slimes.xyz/meta.png" />
+      <meta property="twitter:url" content={`https://www.slimes.xyz/`} />
       {/* <!-- Open Graph / Facebook --> */}
       <meta property="og:type" content="website" />
       <meta property="og:url" content="https://www.slimes.xyz/" />
