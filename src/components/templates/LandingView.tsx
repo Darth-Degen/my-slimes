0e8edee410249130f1554daf50434b0933e35e0e
Darth Degen <darthdegen@gmail.com>
2023-06-06 13:27:18 -0700

    added videos

--- src/components/templates/LandingView.tsx
+++ src/components/templates/LandingView.tsx
@@ -10,20 +10,38 @@ import {
 } from "react";
 import { FlowerVector, CrackVector, PaddleVector } from "@components";
 import { useWindowSize } from "@hooks";
+
+interface Assets {
+  src: string;
+}
+const _assets: Assets[] = [
+  {
+    src: "/videos/loading-intro.mp4",
+  },
+  {
+    src: "/videos/loading-loop.mp4",
+  },
+];
+
 interface Props {
   setAssets?: Dispatch<SetStateAction<boolean[]>>;
   setIsInView: Dispatch<SetStateAction<boolean>>;
 }
+
 const LandingView: FC<Props> = (props: Props) => {
   const { setAssets, setIsInView } = props;
+
+  const [video, setVideo] = useState<string>(_assets[0].src);
+
+  const isLooping = video.includes("loop");
 
   const [winWidth, winHeight] = useWindowSize();
   const { scrollY, scrollYProgress } = useScroll();
   const scrollRef = useRef(null);
 
-  const y = useTransform(scrollY, [0, 400], [0, -400]);
-  const y2 = useTransform(scrollY, [0, 150], [0, -150]);
-  const opacity = useTransform(scrollYProgress, [0, 1], [1, 0]);
+  // const y = useTransform(scrollY, [0, 400], [0, -400]);
+  // const y2 = useTransform(scrollY, [0, 150], [0, -150]);
+  // const opacity = useTransform(scrollYProgress, [0, 1], [1, 0]);
 
   const isInView = useInView(scrollRef);
 
@@ -40,24 +58,24 @@ const LandingView: FC<Props> = (props: Props) => {
       ref={scrollRef}
     >
       {/* graphics */}
-      <div className="absolute left-1/2 top-[46%] transform -translate-x-1/2 -translate-y-1/2 z-10">
+      {/* <div className="absolute left-1/2 top-[46%] transform -translate-x-1/2 -translate-y-1/2 z-10">
         <FlowerVector className="w-[280px] h-[280px] md:w-[380px] md:h-[380px] absolute left-1/2 top-[46%] transform -translate-x-1/2 -translate-y-1/2" />
         <CrackVector className="w-[340px] h-[340px] md:w-[520px] md:h-[520px] absolute left-1/2 top-[46%] transform -translate-x-1/2 -translate-y-1/2" />
         <PaddleVector className="w-[420px] h-[420px] md:w-[560px] md:h-[560px] absolute left-1/2 top-[46%] transform -translate-x-1/2 -translate-y-1/2" />
-      </div>
-      {/* shadow */}
-      <div className="w-[80px] h-[3px] z-10 bg-shadow shadow-circular mb-20 lg:mb-12 rounded-full lg:absolute lg:left-1/2 lg:top-[83%] 3xl:top-[79%] lg:transform lg:-translate-x-1/2 lg:-translate-y-1/2"></div>
-      {/*  footer */}
-      <motion.div
-        className="uppercase font-black text-lg pb-6 3xl:pb-20"
-        // style={{ y: y2 }}
+      </div> */}
+      {/* <div className="w-[80px] h-[3px] z-10 bg-shadow shadow-circular mb-20 lg:mb-12 rounded-full lg:absolute lg:left-1/2 lg:top-[83%] 3xl:top-[79%] lg:transform lg:-translate-x-1/2 lg:-translate-y-1/2"></div> */}
+
+      <video
+        className="h-screen w-screen"
+        autoPlay
+        muted
+        style={{ objectFit: "cover" }}
       >
-        scroll
-      </motion.div>
-      <div
-        className="hidden lg:flex justify-between w-full pb-4 px-4 sm:px-6"
-        // style={{ y, opacity }}
-      >
+        <source src={video} type="video/mp4" />
+      </video>
+
+      <div className="uppercase font-black text-lg pb-6 3xl:pb-20">scroll</div>
+      <div className="hidden lg:flex justify-between w-full pb-4 px-4 sm:px-6">
         <div className="w-1/3 uppercase">the whole squad here</div>
         <div className="w-1/3 uppercase flex justify-center">and</div>
         <div className="w-1/3 uppercase flex justify-end">everybody eats</div>
