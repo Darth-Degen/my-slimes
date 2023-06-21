9806a46c14959f445ed84ff97c3535d869736912
mattdegods <matthewcpfeifer@gmail.com>
2023-06-21 15:25:46 -0400

    Fix: header scrolls in line with page

--- pages/index.tsx
+++ pages/index.tsx
@@ -41,7 +41,7 @@ const Home: NextPage = () => {
   const mobileView = width < 1024;
 
   return (
-    <PageLayout headerType={mobileView ? "fixed" : "scroll"} assets={assets}>
+    <PageLayout headerType={mobileView ? "block" : "scroll"} assets={assets}>
       <IndexView />
     </PageLayout>
   );
