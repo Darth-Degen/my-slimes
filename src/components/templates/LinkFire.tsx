7102f2c2b68afa30c359bc00ac88ac4ebad157dd
Darth Degen <darthdegen@gmail.com>
2023-07-05 15:45:43 -0700

    added linfire extar link

--- src/components/templates/LinkFire.tsx
+++ src/components/templates/LinkFire.tsx
@@ -17,6 +17,18 @@ const LinkFire: FC<Props> = ({ showLoop, setAssets }) => {
         className="md:mt-0 md:mx-10 pb-40 lg:mx-[10%] xl:mx-[20%]"
       >
         <div className="w-full grid grid-cols-2 lg:grid-cols-3 gap-2 md:gap-4 px-5 pb-10 items-center">
+          <div className="col-span-2 lg:hidden w-full h-full overflow-hidden rounded-xl flex justify-center self-center">
+            <MobileLink
+              setAssets={setAssets}
+              href="mint"
+              image={`${process.env.NEXT_PUBLIC_CDN_URL}/images/linkfire/mint.png`}
+              width={4008 / 3.61}
+              height={1002 / 3.61}
+              alt="slimes family gallery link"
+              index={6}
+              isInternal
+            />
+          </div>
           <div className="col-span-1 w-full h-full overflow-hidden rounded-xl">
             <MobileLink
               setAssets={setAssets}
